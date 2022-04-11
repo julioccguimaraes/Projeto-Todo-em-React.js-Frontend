53c092c9b59f4203771784c2994e1b84d241760c
Júlio Guimarães <julioguimaraes@gmail.com>
2022-04-11 15:10:40 -0300

    first commit

---
+++ src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, dolorem velit laudantium saepe voluptatem sunt, vel id voluptas in ex sit sapiente omnis ipsam odio repellat nemo eius maxime error?</p>
+        
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non aperiam hic vel dolor, quod tenetur iusto, sequi alias consectetur amet iure. Non quis porro voluptas soluta nemo ratione sunt dolor?</p>
+        
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda quasi vitae ea dicta dolore accusantium earum quos neque et nam dolor suscipit, vero molestias saepe eaque omnis deserunt repudiandae labore.</p>
+    </div>
+)
